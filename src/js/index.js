6325b0d79dc20163f119b076afecfb606c102fbf
Alex González <alexgm@MacBook-Pro-de-Alex.local>
2018-05-31 00:14:01 +0200

    Add lib css hover efects Title SVG Button start

--- src/js/index.js
+++ src/js/index.js
@@ -9,31 +9,66 @@ window.onload = function() {
 
     new BackGrounds(canvasFloor, canvasSky, canvasCityMiddle, canvasMoon).start();
 
-    $(".start-view").hide();
-    new Game(canvasGame).start();
+    
+    //new Game(canvasGame).start();
     //new BackGrounds(canvasFloor, canvasSky, canvasCityMiddle, canvasMoon).start();
 
+    $( ".start-game-button" ).on( "click", function() {
+      //if(selectPlayer){   
+        $(".start-view").hide();
+     // }
+    });
+
+    onKeyPress();
+    onClickButtons();
+  };
 
 
-    $( ".start-game-button button" ).on( "click", function() {
-      if(selectPlayer){   
-        
+function onKeyPress(){
+    $(document).keydown(function (e) {
+      switch(e.keyCode) {
+        case 37:
+          selectLeftChracter();
+          break;
+        case 39:
+        console.log("danjkdbakj")
+          selectRighthracter();
+          break;
       }
     });
+};
+
+function selectLeftChracter(){
+  //$(".character-one-button").on( "click", function() {
+    selectPlayer = 'robot';
+    $('.character-one').addClass('selected-player');
+    $('.character-two').removeClass('selected-player');
+ // });
+};
+
+function selectRighthracter(){
+  //$(".character-two-button").on( "click", function() {
+    selectPlayer = 'human';
+    $('.character-two').addClass('selected-player');
+    $('.character-one').removeClass('selected-player');
+ // });
+};
+
+function onClickButtons(){
+  selectCharacter();
+};
+
+function selectCharacter(){
+  $('.character-one').click(function (e) {
+    selectLeftChracter();
+  });
+
+  $('.character-two').click(function (e) {
+    selectRighthracter();
+  });
+};
 
 
-    $(".character-one-button").on( "click", function() {
-      selectPlayer = 'robot';
-      $('.character-one').addClass('selected-player');
-      $('.character-two').removeClass('selected-player');
-    });
-    
-    $(".character-two-button").on( "click", function() {
-      selectPlayer = 'human';
-      $('.character-two').addClass('selected-player');
-      $('.character-one').removeClass('selected-player');
-    });
 
 
-
-  };
+  
